9942abe857b6e2a3da6e267242f8b5469f303196
Ken Alex Fassone <nexusventuri@gmail.com>
2019-02-16 17:12:22 +0000

    Set up project, created test runner

---
+++ runtest.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+echo 'Running specs'
+rspec spec/**/*spec.rb
+
+set +e
+output_file='spec/fixture/out.txt'
+ruby spec/texttest_fixture.rb > "$output_file"
+diff spec/fixture/original_output.txt "$output_file"
+
+if [[ $? == 1 ]]; then
+  echo 'fixture failed'
+  exit 1
+else
+  echo 'passed'
+  exit 0
+fi
+
