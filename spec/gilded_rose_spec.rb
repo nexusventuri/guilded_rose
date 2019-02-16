7cb73d2c5cc64039e4e8757995de0b6092fc5289
Ken Alex Fassone <nexusventuri@gmail.com>
2019-02-16 17:53:47 +0000

    Refactor tests

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -4,69 +4,60 @@ describe GildedRose do
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      items = wait_two_days([Item.new("foo", 0, 0)])
       expect(items[0].name).to eq "foo"
     end
   end
 
   context 'Aged brie' do
-    it 'Increases in quality every day' do
-      items = [Item.new('Aged Brie', sell_in = 3, quality=1)]
-      rose = GildedRose.new(items)
-      rose.update_quality()
-      rose.update_quality()
+    it 'increases in quality every day' do
+      items = wait_two_days([Item.new('Aged Brie', sell_in = 3, quality=1)])
       expect(items[0].quality).to eq 3
     end
 
-    it 'Does not increase the quality over 50' do
-      items = [Item.new('Aged Brie', sell_in = 0, quality=49)]
-      rose = GildedRose.new(items)
-      rose.update_quality()
-      rose.update_quality()
+    it 'does not increase the quality over 50' do
+      items = wait_two_days([Item.new('Aged Brie', sell_in = 0, quality=49)])
       expect(items[0].quality).to eq 50
     end
 
-    it 'Increases in quality twice as fast after sell in date' do
-      items = [Item.new('Aged Brie', sell_in = 0, quality=1)]
-      rose = GildedRose.new(items)
-      rose.update_quality()
-      rose.update_quality()
+    it 'increases in quality twice as fast after sell in date' do
+      items = wait_two_days([Item.new('Aged Brie', sell_in = 0, quality=1)])
       expect(items[0].quality).to eq 5
     end
   end
 
   context 'Generic item' do
-    it 'Decreases in quality' do
-      items = [Item.new('Generic item', sell_in = 3, quality=20)]
-      rose = GildedRose.new(items)
-      rose.update_quality()
-      rose.update_quality()
+    it 'decreases in quality' do
+      items = wait_two_days([Item.new('Generic item', sell_in = 3, quality=20)])
       expect(items[0].quality).to eq 18
     end
 
-    it 'After sell in date decreases in quality twice as fast' do
-      items = [Item.new('Generic item', sell_in = 0, quality=20)]
-      rose = GildedRose.new(items)
-      rose.update_quality()
-      rose.update_quality()
+    it 'after sell in date decreases in quality twice as fast' do
+      items = wait_two_days([Item.new('Generic item', sell_in = 0, quality=20)])
       expect(items[0].quality).to eq 16
     end
 
-    it 'Does not decrease in quality below 0' do
-      items = [Item.new('Generic item', sell_in = 0, quality=1)]
-      rose = GildedRose.new(items)
-      rose.update_quality()
-      rose.update_quality()
+    it 'does not decrease in quality below 0' do
+      items = wait_two_days([Item.new('Generic item', sell_in = 0, quality=1)])
       expect(items[0].quality).to eq 0
     end
 
-    it 'Can have negative sell in date' do
-      items = [Item.new('Generic item', sell_in = 0, quality=1)]
-      rose = GildedRose.new(items)
-      rose.update_quality()
-      rose.update_quality()
+    it 'can have negative sell in date' do
+      items = wait_two_days([Item.new('Generic item', sell_in = 0, quality=1)])
       expect(items[0].sell_in).to eq(-2)
     end
+  end
+
+  xcontext 'Sulfuras' do
+    it 'does not change in value' do
+
+    end
+  end
+
+  def wait_two_days(items)
+    rose = GildedRose.new(items)
+    rose.update_quality()
+    rose.update_quality()
+    items
   end
 end
