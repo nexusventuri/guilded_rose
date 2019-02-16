96f391e8f731abfa65bb10b4d78a289d50c0d9a9
Ken Alex Fassone <nexusventuri@gmail.com>
2019-02-16 20:27:54 +0000

    Separate brie and backstage quality update

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -9,8 +9,10 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       next if item.name == "Sulfuras, Hand of Ragnaros"
-      if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert"
-        update_brie_or_concert_quality(item)
+      if item.name == "Aged Brie"
+        update_brie_quality(item)
+      elsif item.name == "Backstage passes to a TAFKAL80ETC concert"
+        update_backstage_concert_quality(item)
       else
         if item.quality > 0
           item.quality = item.quality - 1
@@ -37,10 +39,8 @@ class GildedRose
     end
   end
 
-  def update_brie_or_concert_quality(item)
+  def update_brie_quality(item)
     item.quality = item.quality + 1
-    update_backstage_concert_quality(item)
-
     if item.quality > 50
       item.quality = 50
     end
@@ -48,10 +48,16 @@ class GildedRose
 
   def update_backstage_concert_quality(item)
     if item.name == "Backstage passes to a TAFKAL80ETC concert"
+      item.quality = item.quality + 1
+
       if item.sell_in < 6
         item.quality = item.quality + 2
       elsif item.sell_in < 11
         item.quality = item.quality + 1
+      end
+
+      if item.quality > 50
+        item.quality = 50
       end
     end
   end
