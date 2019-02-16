edabacee5b7fbbfe50cd249417002a309a72b27b
Ken Alex Fassone <nexusventuri@gmail.com>
2019-02-16 21:32:55 +0000

    Add readme, prettify test runner

--- runtest.sh
+++ runtest.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+function red {
+  echo -e "\033[0;31m$1\033[0m"
+}
+
+function green {
+  echo -e "\033[0;32m$1\033[0m"
+}
+
 set -e
 echo 'Running specs'
 bundle exec rspec spec/*spec.rb
@@ -9,11 +17,17 @@ output_file='spec/fixture/out.txt'
 bundle exec ruby spec/texttest_fixture.rb > "$output_file"
 diff spec/fixture/original_output.txt "$output_file"
 
+
 if [[ $? != 0 ]]; then
-  echo 'Fixture failed'
+  red '******************'
+  red '* Fixture failed *'
+  red '******************'
   exit 1
 else
-  echo 'Fixture passed'
+  echo ''
+  green '******************'
+  green '* Fixture passed *'
+  green '******************'
   exit 0
 fi
 
