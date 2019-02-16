5e0c0c74d0dd2378af8eddb0289406c67a1303e8
Ken Alex Fassone <nexusventuri@gmail.com>
2019-02-16 20:47:21 +0000

    Replace recurring condition in constrain quality

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -25,21 +25,21 @@ class GildedRose
 
   def update_brie_sell_in(item)
     item.sell_in -= 1
+    item.quality += 1 if item.sell_in < 0
 
-    if item.sell_in < 0 && item.quality < 50
-      item.quality += 1
-    end
+    constrain_item_quality(item)
   end
 
   def constrain_item_quality(item)
     if item.quality > 50
       item.quality = 50
+    elsif item.quality < 0
+      item.quality = 0
     end
   end
 
   def update_brie_quality(item)
     item.quality += 1
-    constrain_item_quality(item)
   end
 
   def update_backstage_concert_quality(item)
@@ -50,30 +50,24 @@ class GildedRose
     elsif item.sell_in < 11
       item.quality += 1
     end
-
-    constrain_item_quality(item)
   end
 
   def update_backstage_concert_sell_in(item)
     item.sell_in -= 1
+    item.quality = 0 if item.sell_in <= 0
 
-    if item.sell_in <= 0
-      item.quality = 0
-    end
+    constrain_item_quality(item)
   end
 
   def update_generic_item_quality(item)
-    if item.quality > 0
-      item.quality -= 1
-    end
+    item.quality -= 1 if item.quality > 0
   end
 
   def update_generic_item_sell_in(item)
     item.sell_in -= 1
+    item.quality -= 1 if item.sell_in < 0
 
-    if item.sell_in < 0 && item.quality > 0
-      item.quality -= 1
-    end
+    constrain_item_quality(item)
   end
 end
 
