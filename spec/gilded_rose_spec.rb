a046932b59896154d6bebdf4ef0b31f0bc192384
Ken Alex Fassone <nexusventuri@gmail.com>
2019-02-16 17:42:59 +0000

    Add tests for Aged Brie

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -10,4 +10,29 @@ describe GildedRose do
     end
   end
 
+  context 'Aged brie' do
+    it 'Increases in quality every day' do
+      items = [Item.new('Aged Brie', sell_in = 3, quality=1)]
+      rose = GildedRose.new(items)
+      rose.update_quality()
+      rose.update_quality()
+      expect(items[0].quality).to eq 3
+    end
+
+    it 'Does not increase the quality over 50' do
+      items = [Item.new('Aged Brie', sell_in = 0, quality=49)]
+      rose = GildedRose.new(items)
+      rose.update_quality()
+      rose.update_quality()
+      expect(items[0].quality).to eq 50
+    end
+
+    it 'Increases in quality twice as fast after sell in date' do
+      items = [Item.new('Aged Brie', sell_in = 0, quality=1)]
+      rose = GildedRose.new(items)
+      rose.update_quality()
+      rose.update_quality()
+      expect(items[0].quality).to eq 5
+    end
+  end
 end
