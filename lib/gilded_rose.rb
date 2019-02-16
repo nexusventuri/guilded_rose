433a0e6138ddd8e404c6918683a81a1b1c7109ec
Ken Alex Fassone <nexusventuri@gmail.com>
2019-02-16 18:17:07 +0000

    Refactor, make backstage update more explicit

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -32,7 +32,7 @@ class GildedRose
               end
             end
           else
-            item.quality = item.quality - item.quality
+            item.quality = 0
           end
         else
           if item.quality < 50
